0a2726552799761d113a309587c2945725a6a548
Bernhard Schuster <bernhard@ahoi.io>
2023-01-13 22:29:23 +0100

    fix/tectonic: adjust textcase

--- mdbook-tectonic/src/tests.rs
+++ mdbook-tectonic/src/tests.rs
@@ -1,30 +1,30 @@
 use super::*;
-use std::fs::OpenOptions;
 use std::path::PathBuf;
 
 #[test]
-fn test_traverse_markdown() {
-    let imgpath = Path::new("/tmp/test/src/chap/xyz.png");
+fn test_traverse_markdown() -> Result<(), Box<dyn std::error::Error>> {
+    let imagepath = Path::new("/tmp/test/images/chap/foo.png");
     // create a temporary directory in /tmp/
-    fs::create_dir_all(imgpath.parent().unwrap()).expect("failure while creating testdirs");
-    // touch the mock png file
-    let _ = match OpenOptions::new().create(true).write(true).open(imgpath) {
-        Ok(_) => Ok(()),
-        Err(e) => Err(e),
-    };
-    let content = "![123](./xyz.png)";
-    let path = PathBuf::from(r"chap/");
+    fs::create_dir_all(imagepath.parent().unwrap()).expect("failure while creating testdirs");
+    // touch the mock png file;
+    fs::copy(
+        PathBuf::from(env!("CARGO_MANIFEST_DIR")).join("mdbook-latex.png"),
+        imagepath,
+    )?;
+
+    let path = PathBuf::from(r"chap");
     let context = RenderContext::new(
         Path::new("/tmp/test/"),
         mdbook::book::Book::new(),
         mdbook::Config::default(),
         Path::new("/tmp/dest/"),
     );
-    let new_content = traverse_markdown(content, &path, &context).unwrap();
-    assert_eq!("![123](images/chap/xyz.png)", new_content);
-    let respath = Path::new("/tmp/dest/images/chap/xyz.png");
+    let new_content = traverse_markdown("![123](images/chap/foo.png)", &path, &context).unwrap();
+    assert_eq!("![123](images/chap/foo.png)", new_content);
+    let respath = Path::new("/tmp/dest/images/chap/foo.png");
     assert!(respath.exists());
 
-    fs::remove_dir_all("/tmp/test").unwrap();
-    fs::remove_dir_all("/tmp/dest").unwrap();
+    fs::remove_dir_all("/tmp/test")?;
+    fs::remove_dir_all("/tmp/dest")?;
+    Ok(())
 }
